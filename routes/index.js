f1bf26a5e9b02a737d2f404a82b46ad8df709620
Souler <soulerhyd@gmail.com>
2015-04-28 22:42:26 +0200

    added automatic routes based on views diectory structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
+var auto = function(req, res, next) {
+	var app = req.app;
+	var p = req.path.substring(1);
+	if (p.length <= 0)
+		p = 'index';
+
+	var file = path.join(app.get('views'), p + '.' + app.get('view engine'));
+
+	if (fs.existsSync(file))
+		res.render(p);
+	else
+		next();
+}
+
+router.get('*', auto);
 
 module.exports = router;
